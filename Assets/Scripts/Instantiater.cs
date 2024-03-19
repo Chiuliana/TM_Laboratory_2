d77b3c193502f0eb263fa151220f3cf9ef0791e9
Chiuliana <chichioiuliana@gmail.com>
2024-03-19 15:26:24 +0200

    initial and last commit

---
+++ Assets/Scripts/Instantiater.cs
@@ -0,0 +1,485 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Instantiater : MonoBehaviour
+{   
+    public GameObject cellTemplate;
+    public GameObject cellGod;
+    public GameObject cellDiablo;
+    public GameObject cellIntr;
+
+
+
+    float generationInterval = 0.2f;
+    public static bool pause = false;
+
+    int[ , ] cellsArray;
+
+    public int gridHeight;
+    private int gridWidth;
+
+    public GameObject FertileZone;
+    public GameObject DeadZone;
+    Vector3 fertilePos;
+    Vector3 deadPos;
+
+    public static bool zones = true;
+
+
+    private Camera cam;
+
+    private float cellSize;
+
+    // Start is called before the first frame update
+    void Start(){
+        gridWidth = Mathf.RoundToInt(gridHeight * Camera.main.aspect);
+
+        cellsArray = new int[gridHeight, gridWidth];
+        cellSize = (Camera.main.orthographicSize * 2) / gridHeight;
+        print ("Dimensions: " + gridWidth + "X" + gridHeight);
+        print("Cell size: " + cellSize);
+
+        FertileZone = GameObject.Find("FertileZone");
+        fertilePos = FertileZone.transform.position;
+        print(fertilePos.x + " " + fertilePos.y);
+
+        DeadZone = GameObject.Find("DeadZone");
+        deadPos = DeadZone.transform.position;
+        print(deadPos.x + " " + deadPos.y);
+
+        // fill random alive cells
+        for (int i = 0; i < 1500; i++){
+            cellsArray[Random.Range(20, 70), Random.Range(30, 120)] = 1; 
+        }
+
+        cam = Camera.main;
+
+        InvokeRepeating("NewGenerationUpdate", generationInterval, generationInterval);
+    }
+
+
+    void NewGenerationUpdate(){
+        if (Input.GetMouseButton(2))
+        {
+            if (pause==true) pause = false; else pause = true;
+            print(" Pause " + pause);
+        }
+        else
+        {           
+            if (pause==false)
+            {
+                RenderCells();
+                ApplyRules();
+                //Environment();
+
+            }
+            UserInput();
+        }
+    }
+
+    void UserInput()
+    {
+        if (Input.GetKey("p"))
+        {
+            
+            if (pause == true) pause = false; else pause = true;
+            print(" Pause " + pause);
+            
+        } else if (Input.GetMouseButton(0))
+        {
+            Vector3 point = new Vector3();
+            Vector3 mousePos = Input.mousePosition;
+            {
+                mousePos.y = cam.pixelHeight - mousePos.y;
+                point = cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, cam.nearClipPlane));
+
+                int x = 0;
+                int y = 0;
+                x = (int)((point.x - cellSize / 2) / cellSize);
+                y = (int)((point.y - cellSize / 2) / cellSize);
+
+                print(" x= " + x);
+                print(" y= " + y);
+                if (x > 0 && x < gridWidth && y > 0 && y < gridHeight) cellsArray[y, x] = 1;
+                if(zones)UserEnvironment(x, y);
+            }
+            RenderCells();
+        }
+        else if (Input.GetMouseButton(1))
+        {
+            Vector3 point = new Vector3();
+            Vector3 mousePos = Input.mousePosition;
+            {
+                mousePos.y = cam.pixelHeight - mousePos.y;
+                point = cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, cam.nearClipPlane));
+
+                int x = 0;
+                int y = 0;
+                x = (int)((point.x - cellSize / 2) / cellSize);
+                y = (int)((point.y - cellSize / 2) / cellSize);
+
+                print(" x= " + x);
+                print(" y= " + y);
+                if (x > 0 && x < gridWidth && y > 0 && y < gridHeight) cellsArray[y, x] = 0;
+                if(zones)UserEnvironment(x, y);
+            }
+            RenderCells();
+        }
+        else if (Input.GetKey("o"))
+        {
+            Vector3 point = new Vector3();
+            Vector3 mousePos = Input.mousePosition;
+            {
+                mousePos.y = cam.pixelHeight - mousePos.y;
+                point = cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, cam.nearClipPlane));
+
+                int x = 0;
+                int y = 0;
+                x = (int)((point.x - cellSize / 2) / cellSize);
+                y = (int)((point.y - cellSize / 2) / cellSize);
+
+                print(" x= " + x);
+                print(" y= " + y);
+
+                for(int i = y - 10; i <= y + 10; i++)
+                {
+                    for (int j = x - 10; j <= x + 10; j++)
+                    {
+                        if (j > 0 && j < gridWidth && i > 0 && i < gridHeight) cellsArray[i, j] = 1;
+                    }
+                }
+
+                if(zones)UserEnvironment(x, y);
+            }
+            RenderCells();
+        }
+        else if (Input.GetKey("g"))
+        {
+            Vector3 point = new Vector3();
+            Vector3 mousePos = Input.mousePosition;
+            {
+                mousePos.y = cam.pixelHeight - mousePos.y;
+                point = cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, cam.nearClipPlane));
+
+                int x = 0;
+                int y = 0;
+                x = (int)((point.x - cellSize / 2) / cellSize);
+                y = (int)((point.y - cellSize / 2) / cellSize);
+
+                print(" x= " + x);
+                print(" y= " + y);
+                if (x > 0 && x < gridWidth && y > 0 && y < gridHeight) cellsArray[y, x] = 2;
+                print("God cell");
+                if (zones) UserEnvironment(x, y);
+            }
+            RenderCells();
+        }
+        else if (Input.GetKey("d"))
+        {
+            Vector3 point = new Vector3();
+            Vector3 mousePos = Input.mousePosition;
+            {
+                mousePos.y = cam.pixelHeight - mousePos.y;
+                point = cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, cam.nearClipPlane));
+
+                int x = 0;
+                int y = 0;
+                x = (int)((point.x - cellSize / 2) / cellSize);
+                y = (int)((point.y - cellSize / 2) / cellSize);
+
+                print(" x= " + x);
+                print(" y= " + y);
+                if (x > 0 && x < gridWidth && y > 0 && y < gridHeight) cellsArray[y, x] = 3;
+                print("Diablo cell");
+                if (zones) UserEnvironment(x, y);
+            }
+            RenderCells();
+        }
+    }
+
+
+    void RenderCells(){
+        foreach (GameObject cell in GameObject.FindGameObjectsWithTag("Cell")){
+            Destroy(cell);
+        }
+
+        for (int i = 0; i < gridHeight; i++){
+            for (int j = 0; j < gridWidth; j++){
+                if (cellsArray[i, j] == 0) continue;
+                if (cellsArray[i, j] == 1)
+                {
+                    Vector3 cellPosition = new Vector3(
+                        j * cellSize + cellSize / 2,
+                        (cellSize * gridHeight) - (i * cellSize + cellSize / 2),
+                        0
+                    );
+                    Instantiate(cellTemplate, cellPosition, Quaternion.identity);
+                }
+                if (cellsArray[i, j] == 2)
+                {
+                    Vector3 cellPosition = new Vector3(
+                        j * cellSize + cellSize / 2,
+                        (cellSize * gridHeight) - (i * cellSize + cellSize / 2),
+                        0
+                    );
+                    Instantiate(cellGod, cellPosition, Quaternion.identity);
+                }
+                if (cellsArray[i, j] == 3)
+                {
+                    Vector3 cellPosition = new Vector3(
+                        j * cellSize + cellSize / 2,
+                        (cellSize * gridHeight) - (i * cellSize + cellSize / 2),
+                        0
+                    );
+                    Instantiate(cellDiablo, cellPosition, Quaternion.identity);
+                }
+            }
+        }
+
+    }
+
+    void ApplyRules()
+    {
+
+        FertileZone = GameObject.Find("FertileZone");
+        fertilePos = FertileZone.transform.position;
+        DeadZone = GameObject.Find("DeadZone");
+        deadPos = DeadZone.transform.position;
+
+        int[,] nextGenGrid = new int[gridHeight, gridWidth];
+        for (int i = 0; i < gridHeight; i++)
+        {
+            for (int j = 0; j < gridWidth; j++)
+            {
+                int livingNeighbours = CountLivingNeighbours(i, j);
+
+
+                if (ControlGod(i, j))
+                {
+                    nextGenGrid[i - 1, j] = 1;
+                    nextGenGrid[i, j - 1] = 1;
+                    nextGenGrid[i, j + 1] = 1;
+                    nextGenGrid[i + 1, j] = 1;
+                    nextGenGrid[i, j] = 2;
+                }
+                else if (ControlDiablo(i, j))
+                {
+                    nextGenGrid[i - 1, j] = 0;
+                    nextGenGrid[i, j - 1] = 0;
+                    nextGenGrid[i, j + 1] = 0;
+                    nextGenGrid[i + 1, j] = 0;
+                    cellsArray[i - 1, j] = 0;
+                    cellsArray[i, j - 1] = 0;
+                    cellsArray[i, j + 1] = 0;
+                    cellsArray[i + 1, j] = 0;
+
+                    nextGenGrid[i, j] = 3;
+                }
+                else if (livingNeighbours == 3 ) 
+                { // reproduction, exactly 3 neighgbours
+                    nextGenGrid[i, j] = 1;
+                }
+                else if (livingNeighbours == 2 && cellsArray[i, j] == 1)
+                { // exactly 2 neigh, the live cell survives
+                    nextGenGrid[i, j] = 1;
+                }
+
+
+                if (zones)
+                {
+                    bool fertile = PosFertile(j * cellSize + cellSize / 2, (cellSize * gridHeight) - (i * cellSize + cellSize / 2));
+                    bool dead = PosDead(j * cellSize + cellSize / 2, (cellSize * gridHeight) - (i * cellSize + cellSize / 2));
+                    if (dead)
+                    {
+                        //print(i + " " + j + " dead");
+                        nextGenGrid[i, j] = 0;
+                    }
+                    else
+                        if (fertile)
+                    {
+                        if (livingNeighbours == 2 || livingNeighbours == 3)
+                        { // reproduction,  2 to 3 neighgbours
+                            nextGenGrid[i, j] = 1;
+                        }
+                        else if ((livingNeighbours == 1 || livingNeighbours == 2) && cellsArray[i, j] == 1)
+                        { // 1 or 2 neigh, the live cell survives
+                            nextGenGrid[i, j] = 1;
+                        }
+                    }
+                }
+            }
+        }
+        cellsArray = nextGenGrid; // GOING TO THE NEXT GEN!
+    }
+
+    void UserEnvironment(int x, int y)
+    {//fertile zone
+        if(x > 5 && x<20 && y > 5 && y < 20)
+        {
+            for(int i = 5; i <= 20; i++)
+                for(int j = 5; j <= 20; j++)
+                {
+                    cellsArray[j, i] = 1;
+                }
+        }
+
+        //dead zone
+        if (x > 5 && x < 20 && y > 50 && y < 70)
+        {
+            for (int i = 5; i <= 20; i++)
+                for (int j = 50; j <= 70; j++)
+                {
+                    cellsArray[j, i] = 0;
+                }
+            //print(" Dead Zone ");
+        }
+
+        //meaw zone
+        if (x > 130 && x < 153 && y > 70 && y < 80)
+        {
+            for (int i = 130; i <= 153; i++)
+                for (int j = 70; j <= 80; j++)
+                {
+                    cellsArray[j, i] = 0;
+                }
+
+            for (int j = 71; j <= 75; j++)
+            {
+                cellsArray[j, 131] = 1;
+                cellsArray[j, 135] = 1;
+                cellsArray[j, 137] = 1;
+            }
+            cellsArray[72, 132] = 1;
+            cellsArray[72, 134] = 1;
+            cellsArray[73, 133] = 1;
+            cellsArray[71, 138] = 1;
+            cellsArray[71, 139] = 1;
+            cellsArray[73, 138] = 1;
+            cellsArray[75, 138] = 1;
+            cellsArray[75, 139] = 1;
+            cellsArray[75, 141] = 1;
+            cellsArray[74, 141] = 1;
+            cellsArray[74, 143] = 1;
+            cellsArray[73, 142] = 1;
+            cellsArray[72, 142] = 1;
+            cellsArray[71, 143] = 1;
+            cellsArray[73, 144] = 1;
+            cellsArray[72, 144] = 1;
+            cellsArray[75, 145] = 1;
+            cellsArray[74, 145] = 1;
+            cellsArray[74, 147] = 1;
+            cellsArray[73, 147] = 1;
+            cellsArray[72, 147] = 1;
+            cellsArray[71, 147] = 1;
+            cellsArray[75, 148] = 1;
+            cellsArray[73, 149] = 1;
+            cellsArray[72, 149] = 1;
+            cellsArray[74, 149] = 1;
+            cellsArray[75, 150] = 1;
+            cellsArray[74, 151] = 1;
+            cellsArray[73, 151] = 1;
+            cellsArray[72, 151] = 1;
+            cellsArray[71, 151] = 1;
+            print(" MEAW Zone ");
+        }
+    }
+
+    void Environment()
+    {
+        if (zones)
+        {
+            int[,] nextGenGrid = new int[gridHeight, gridWidth];
+            nextGenGrid = cellsArray;
+            for (int i = 0; i < gridHeight; i++)
+            {
+                for (int j = 0; j < gridWidth; j++)
+                {
+                    bool fertile = PosFertile(j * cellSize + cellSize / 2, (cellSize * gridHeight) - (i * cellSize + cellSize / 2));
+                    bool dead = PosDead(j * cellSize + cellSize / 2, (cellSize * gridHeight) - (i * cellSize + cellSize / 2));
+                    int livingNeighbours = CountLivingNeighbours(i, j);
+
+                    if (livingNeighbours == 3)
+                    { // reproduction, exactly 3 neighgbours
+                        nextGenGrid[i, j] = 1;
+                    }
+                    else if (livingNeighbours == 2 && cellsArray[i, j] == 1)
+                    { // exactly 2 neigh, the live cell survives
+                        nextGenGrid[i, j] = 1;
+                    }
+
+                    if (dead)
+                    {
+                        print(i + " " + j);
+                        nextGenGrid[i, j] = 0;
+                    }
+                    else
+                    if (fertile)
+                    {
+                        if (livingNeighbours == 2 || livingNeighbours == 3)
+                        { // reproduction,  2 to 3 neighgbours
+                            nextGenGrid[i, j] = 1;
+                        }
+                        else if ((livingNeighbours == 1 || livingNeighbours == 2) && cellsArray[i, j] == 1)
+                        { // 1 or 2 neigh, the live cell survives
+                            nextGenGrid[i, j] = 1;
+                        }
+                    }
+                }
+            }
+
+            cellsArray = nextGenGrid; // GOING TO THE NEXT GEN!
+        }
+    }
+
+    bool Control(int x,int y)
+    {
+        if (x > 0 && x < gridWidth && y > 0 && y < gridHeight) return true; else return false;
+    }
+
+    bool PosFertile(float x,float y)
+        {
+
+        if (x > fertilePos.x - 3.1 && x < fertilePos.x + 3.1 && y > fertilePos.y - 1 && y < fertilePos.y + 1) {
+            //print("fertile"); 
+            //print(fertilePos.x + " " + fertilePos.y);
+            return true; } else return false;
+        }
+
+    bool PosDead(float x,float y)
+        {
+        if (x > deadPos.x - 3.1 && x < deadPos.x + 3.1 && y > deadPos.y - 1 && y < deadPos.y + 1) { 
+            //print(x + " " + y + " "  + deadPos.x  +  " dead"); 
+            return true; } else return false;
+    }
+
+    bool ControlGod(int x , int y)
+    {
+        if (cellsArray[x, y] == 2) return true;
+        return false;
+    }
+
+    bool ControlDiablo(int x,int y)
+    {
+        if (cellsArray[x, y] == 3) return true;
+        return false;
+    }
+
+
+    int CountLivingNeighbours(int i, int j){
+        int result = 0;
+        for (int iNeigh = i-1; iNeigh < i+2; iNeigh++){ // i-1, i, i+1
+            for (int jNeigh = j-1; jNeigh < j+2; jNeigh++){ // j-1, j, j+1
+                if (iNeigh == i && jNeigh == j) continue;
+                try{
+                    result += cellsArray[iNeigh, jNeigh];
+                }
+                catch{}
+            }
+        }
+
+        return result;
+    }
+
+}
